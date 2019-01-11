188ad4d9782b25d7349258bee876d1f5c1f4e84a
ja9-look <janineluk95@gmail.com>
2019-01-11 14:08:35 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,46 @@ const questions = [
 ]
 
 let question;
+
+function appendQuestion(question){
+  let containerEl = document.querySelector('.question-container')
+  return containerEl.innerHTML = (question['questionText'])
+}
+
+function askQuestionThen(time){
+  question = questions[0]
+  appendQuestion(question)
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(question)
+    }, time)
+  })
+}
+
+function removeQuestion() {
+  return new Promise(() => {
+    document.querySelector('.question-container').innerHTML = "";
+  })
+}
+
+function askQuestionThenRemoveQuestion(time){
+  return askQuestionThen(time).then(removeQuestion)
+}
+
+function trueAndFalseButtons(){
+  return document.querySelector('.true-false-list').querySelectorAll('.btn')
+}
+
+function toggleTrueAndFalseButtons(){
+    trueAndFalseButtons().forEach((button) => {
+      button.classList.toggle("hide")
+    })
+}
+
+function displayQuestionOnClick(){
+  let askBtn = document.querySelector('a')
+  return askBtn.addEventListener('click', () => {
+    toggleTrueAndFalseButtons()
+    askQuestionThenRemoveQuestion(5000)
+  })
+}
